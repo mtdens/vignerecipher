8ca90661ebebd066611b8a5dbbad7119a776000c
Matthew <mdenslinger@gmail.com>
2017-12-26 10:41:04 -0500

    Create encrypt and decrypt functions

--- script.js
+++ script.js
@@ -16,4 +16,26 @@ $(document).ready(function(){
 		return $('#key').val();
 	}
 
+	function encrypt(plaintext, key){
+		/*
+		** Takes a plaintext number and a key number and returns their sum
+		** Returns: an integer
+		*/
+		return (plaintext + key) % 26;
+ 	}
+
+ 	function decrypt(ciphertext, key){
+ 		/*
+ 		** Takes a ciphertext number and a key number and returns their differnce
+ 		** Returns: an integer
+ 		*/
+
+ 		if( ciphertext - key < 0){
+ 			return (ciphertext - key + 26) % 26; 
+ 		}
+ 		else{
+ 			return (ciphertext - key) % 26;
+ 		}
+ 	}
+
 });
