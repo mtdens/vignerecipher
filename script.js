4e1a22888c1453806b02c68252c47f553ca4a6f2
Matthew <mdenslinger@gmail.com>
2017-12-25 21:09:53 -0500

    Create get message and get key functions

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	function get_message(){
+		/*
+		** Returns the text from the message textbox
+		** Returns: a string
+		*/
+		return $("#msg1").val();
+	}
+
+	function get_key(){
+		/*
+		** Returns the text from the keyword textbox
+		** Returns: a string
+		*/
+		return $('#key').val();
+	}
+
+});
